266ba7053af6775aa90ce73c79870faaec74d762
Adam Fišer <adam.fiser@wanex.cz>
2024-01-03 17:37:03 +0100

    Create seznam_podminky.py

---
+++ seznam_podminky.py
@@ -0,0 +1,14 @@
+# TODO: Napište program, který zobrazí čísla ze seznamu, která splňují následující podmínky:
+# Hodnota je dělitelná pěti
+# Číslo musí být větší než 150
+# Pokud je číslo větší než 500, zastavte cyklus
+
+numbers = [12, 75, 150, 180, 145, 525, 50]
+
+# Očekávaný výstup:
+# ```
+# 75
+# 150
+# 180
+# 145
+# ```
