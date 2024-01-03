498a1d4f2c04800ad167d9151efb87e0ea8baf35
Adam Fišer <adam.fiser@wanex.cz>
2024-01-03 17:34:39 +0100

    Create soucet_cisel.py

---
+++ soucet_cisel.py
@@ -0,0 +1,3 @@
+# TODO: Od uživatele si vyžádejte číselnou hodnotu. Pomocí cyklu proveďte součet všech čísel od jedné, do zadané hodnoty.
+# Pomocí uživatelské hodnoty předchozího příkladu zjistěte, zda-li je hodnota čísla kladná, nebo záporná.
+
